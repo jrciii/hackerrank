c41eb453a61c3b17d80e0b73698c525c2397a987
jrciii <josephcooperiii@gmail.com>
2021-10-06 19:17:54 -0400

    coin change cleanup

--- coin-change.lisp
+++ coin-change.lisp
@@ -1,16 +1,11 @@
-(defvar *amt* (car (read-from-string (format nil "(~a)" (read-line)))))
-(defvar *denoms* (read-from-string (format nil "(~a)" (read-line))))
-
-(defvar *c* (make-hash-table :test #'equal))
-
 (defun seth (h k v)
   (setf (gethash k h) v))
 
-(defun make-change (amount values)  
+(defun make-change (amount values db)  
   (apply #'+ (mapcar
 	      (lambda (coin)
 		(let ((remain (- amount coin))
-		      (memo (gethash (list amount coin) *c*)))
+		      (memo (gethash (list amount coin) db)))
 		  (cond
 		    (memo memo)
 		    (t
@@ -18,10 +13,15 @@
 		       ((< remain 0) 0)
 		       ((= remain 0) 1)
 		       (t
-			(let ((result (make-change remain (remove-if (lambda (x) (< x coin)) values))))
-			  (seth *c* (list amount coin) result))))))))
+			(let ((result
+				(make-change
+				 remain
+				 (remove-if (lambda (x) (< x coin)) values)
+				 db)))
+			  (seth db (list amount coin) result))))))))
 	      values)))
 
-(setf *c* (make-hash-table :test #'equal))
-
-(format t "~a" (make-change *amt* *denoms*))
+(let ((amt (car (read-from-string (format nil "(~a)" (read-line)))))
+      (denoms (read-from-string (format nil "(~a)" (read-line))))
+      (db (make-hash-table :test #'equal)))
+  (format t "~a" (make-change amt denoms db)))
