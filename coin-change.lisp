7cc4caa9cba64d0a4cb07746656cc137d64aeed3
jrciii <josephcooperiii@gmail.com>
2021-10-06 21:48:12 -0400

    coin change cleanup

--- coin-change.lisp
+++ coin-change.lisp
@@ -1,27 +1,30 @@
-(defun seth (h k v)
-  (setf (gethash k h) v))
+(defun sethash (hash-table key value)
+  (setf (gethash key hash-table) value))
 
-(defun make-change (amount values db)  
-  (apply #'+ (mapcar
-	      (lambda (coin)
-		(let ((remain (- amount coin))
-		      (memo (gethash (list amount coin) db)))
-		  (cond
-		    (memo memo)
-		    (t
-		     (cond
-		       ((< remain 0) 0)
-		       ((= remain 0) 1)
-		       (t
-			(let ((result
-				(make-change
-				 remain
-				 (remove-if (lambda (x) (< x coin)) values)
-				 db)))
-			  (seth db (list amount coin) result))))))))
-	      values)))
+(defun make-change (amount denominations cache)  
+  (apply
+   #'+
+   (mapcar
+    (lambda (coin)
+      (let ((remain (- amount coin))
+        (memo (gethash (list amount coin) cache)))
+    (cond
+      (memo memo)
+      ((< remain 0) 0)
+      ((= remain 0) 1)
+      (t
+       (let ((result
+	       (make-change
+		remain
+		(remove-if (lambda (d) (< d coin)) denominations)
+		cache)))
+         (sethash
+          cache
+          (list amount coin)
+          result))))))
+    denominations)))
 
-(let ((amt (car (read-from-string (format nil "(~a)" (read-line)))))
-      (denoms (read-from-string (format nil "(~a)" (read-line))))
-      (db (make-hash-table :test #'equal)))
-  (format t "~a" (make-change amt denoms db)))
+(let ((amount (car (read-from-string (format nil "(~a)" (read-line)))))
+      (denominations (read-from-string (format nil "(~a)" (read-line))))
+      (cache (make-hash-table :test #'equal)))
+  (format t "~a" (make-change amount denominations cache)))
