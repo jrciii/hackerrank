970a8d692a652c60f840513628c6d646a5f0c69e
jrciii <josephcooperiii@gmail.com>
2021-10-07 10:12:47 -0400

    use 2d array for coin change cache

--- coin-change.lisp
+++ coin-change.lisp
@@ -1,13 +1,10 @@
-(defun sethash (hash-table key value)
-  (setf (gethash key hash-table) value))
-
 (defun make-change (amount denominations cache)  
   (apply
    #'+
    (mapcar
     (lambda (coin)
       (let ((remain (- amount coin))
-            (memo (gethash (list amount coin) cache)))
+            (memo (aref cache amount coin)))
 	(cond
 	  (memo memo)
 	  ((< remain 0) 0)
@@ -18,10 +15,10 @@
 		    remain
 		    (remove-if (lambda (d) (< d coin)) denominations)
 		    cache)))
-             (sethash cache (list amount coin) result))))))
+             (setf (aref cache amount coin) result))))))
     denominations)))
 
-(let ((amount (car (read-from-string (format nil "(~a)" (read-line)))))
-      (denominations (read-from-string (format nil "(~a)" (read-line))))
-      (cache (make-hash-table :test #'equal)))
+(let* ((amount (car (read-from-string (format nil "(~a)" (read-line)))))
+       (denominations (read-from-string (format nil "(~a)" (read-line))))
+       (cache (make-array (list (1+ amount) (1+ (apply #'max denominations))) :initial-element nil)))
   (format t "~a" (make-change amount denominations cache)))
